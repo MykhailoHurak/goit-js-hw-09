b14527305082ba643c0a9d2ac041e28430cf7611
Mikhail Gurak <mikhail.gurak@gmail.com>
2022-06-01 12:14:50 +0300

    task 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,82 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+
+let selectedDate = null;
+let intervalId = null;
+
+refs.startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < Date.now()) {
+      return Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      Notiflix.Notify.success('The selected date is valid!');
+      selectedDate = selectedDates[0].getTime();
+      return (refs.startButton.disabled = false);
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+refs.startButton.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  intervalId = setInterval(() => {
+    const deltaTime = selectedDate - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    if (deltaTime <= 0) {
+      clearInterval(intervalId);
+      return;
+    }
+    refs.startButton.disabled = true;
+    updateTimer({ days, hours, minutes, seconds });
+  }, 1000);
+}
+
+function addZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
 
 // ======================================
 // Завдання 2 - таймер зворотного відліку
